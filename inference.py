adafd704f0d928503adac9378ae84856900b8563
ceciliavision <zhangxer77@gmail.com>
2019-05-05 20:45:56 -0700

    update inference

--- inference.py
+++ inference.py
@@ -36,7 +36,7 @@ def main():
     # set up the model
     with tf.variable_scope(tf.get_variable_scope()):
         input_raw=tf.placeholder(tf.float32,shape=[1,None,None,num_in_ch], name="input_raw")
-        out_rgb = net.SRResnet(input_raw, num_out_ch, up_ratio=up_ratio, reuse=False, up_type=upsample_type, is_training=True)
+        out_rgb = net.SRResnet(input_raw, num_out_ch, up_ratio=up_ratio, reuse=False, up_type=upsample_type)
 
         if raw_tol != 0:
             out_rgb = out_rgb[:,int(raw_tol/2)*(up_ratio*4):-int(raw_tol/2)*(up_ratio*4),
@@ -73,50 +73,59 @@ def main():
         os.makedirs("%s/%s"%(task_folder, mode))
 
     for id, inference_path in enumerate(inference_paths):
-        up_ratio_list = [10,9,8,7,6,5]
-        for up_ratio in up_ratio_list:
-            scale = 10.
-            resize_ratio = up_ratio/scale
+        print("Inference on %d image."%(id+1))
+        scale = 10.
+        crop_ratio_list = [8]
+        fracx = config_file["io"]["fracx"]
+        fracy = config_file["io"]["fracy"]
+        save_prefix = config_file["io"]["prefix"] # 0.35,0.45,0.55,0.65,0.75
+        for idx, fracx in enumerate([0.5]): 
+            for idy, fracy in enumerate([0.5]):
+                save_prefix = "-%d-%d"%(idx,idy)
+                for crop_ratio in crop_ratio_list:
+                    resize_ratio = crop_ratio/scale
 
-            if not os.path.isdir("%s/%s"%(task_folder, mode)):
-                os.makedirs("%s/%s"%(task_folder, mode))
+                    prefix = os.path.basename(os.path.dirname(inference_path))
 
-            if not os.path.isdir("%s/%s/%s-%dx"%(task_folder, mode, up_ratio, id)):
-                os.makedirs("%s/%s/%s-%dx"%(task_folder, mode, up_ratio, id))
+                    if not os.path.isdir("%s/%s"%(task_folder, mode)):
+                        os.makedirs("%s/%s"%(task_folder, mode))
 
-            wb_txt = os.path.dirname(inference_path)+'/wb.txt'
-            out_wb = utils.read_wb(wb_txt, key=os.path.basename(inference_path).split('.')[0]+":")
-            print("white balance: ",out_wb)
+                    if not os.path.isdir("%s/%s/%s-%dx"%(task_folder, mode, prefix, crop_ratio)):
+                        os.makedirs("%s/%s/%s-%dx"%(task_folder, mode, prefix, crop_ratio))
 
-            input_bayer = utils.get_bayer(inference_path, black_lv, white_lv)
-            input_raw_reshape = utils.reshape_raw(input_bayer)
-            input_raw_img_orig = utils.crop_fov(input_raw_reshape, 1./up_ratio)
+                    wb_txt = os.path.dirname(inference_path)+'/wb.txt'
+                    out_wb = utils.read_wb(wb_txt, key=os.path.basename(inference_path).split('.')[0]+":")
+                    print("white balance: ",out_wb)
 
-            rgb_camera_path = inference_path.replace(".ARW",".JPG")
-            rgb_camera =  np.array(Image.open(rgb_camera_path))
-            cropped_input_rgb = utils.crop_fov(rgb_camera, 1./up_ratio)
-            cropped_input_rgb = utils.image_float(cropped_input_rgb)
+                    input_bayer = utils.get_bayer(inference_path, black_lv, white_lv)
+                    input_raw_reshape = utils.reshape_raw(input_bayer)
+                    input_raw_img_orig = utils.crop_fov_free(input_raw_reshape, 1./crop_ratio, crop_fracx=fracx, crop_fracy=fracy)
 
-            print("Testing on image : %s"%(inference_path), input_raw_img_orig.shape)
+                    rgb_camera_path = inference_path.replace(".ARW",".JPG")
+                    rgb_camera =  np.array(Image.open(rgb_camera_path))
+                    cropped_input_rgb = utils.crop_fov_free(rgb_camera, 1./crop_ratio, crop_fracx=fracx, crop_fracy=fracy)
+                    cropped_input_rgb = utils.image_float(cropped_input_rgb)
 
-            input_raw_img = np.expand_dims(input_raw_img_orig, 0)
-            out_objDict=sess.run(objDict,feed_dict={input_raw:input_raw_img})
-            
-            wb_rgb = out_objDict["out_rgb"][0,...]
-            wb_rgb[...,0] *= np.power(out_wb[0,0],1/2.2)
-            wb_rgb[...,1] *= np.power(out_wb[0,1],1/2.2)
-            wb_rgb[...,2] *= np.power(out_wb[0,3],1/2.2)
-            
-            output_rgb = Image.fromarray(np.uint8(utils.clipped(wb_rgb)*255))
-            output_rgb = output_rgb.resize((int(output_rgb.width * resize_ratio),
-                int(output_rgb.height * resize_ratio)), Image.ANTIALIAS)
-            output_rgb.save("%s/%s/%s-%dx/out_rgb.png"%(task_folder,mode,up_ratio,id))
+                    print("Testing on image : %s"%(inference_path), input_raw_img_orig.shape)
 
-            input_camera_rgb = Image.fromarray(np.uint8(utils.clipped(cropped_input_rgb)*255))
-            input_camera_rgb.save("%s/%s/%s-%dx/input_rgb_camera_orig.png"%(task_folder,mode,up_ratio,id))
-            input_camera_rgb_naive = input_camera_rgb.resize((int(input_camera_rgb.width * up_ratio),
-                int(input_camera_rgb.height * up_ratio)), Image.ANTIALIAS)
-            input_camera_rgb_naive.save("%s/%s/%s-%dx/input_rgb_camera_naive.png"%(task_folder,mode,up_ratio,id), compress_level=1)
+                    input_raw_img = np.expand_dims(input_raw_img_orig, 0)
+                    out_objDict=sess.run(objDict,feed_dict={input_raw:input_raw_img})
+                    
+                    wb_rgb = out_objDict["out_rgb"][0,...]
+                    wb_rgb[...,0] *= np.power(out_wb[0,0],1/2.2)
+                    wb_rgb[...,1] *= np.power(out_wb[0,1],1/2.2)
+                    wb_rgb[...,2] *= np.power(out_wb[0,3],1/2.2)
+                    
+                    output_rgb = Image.fromarray(np.uint8(utils.clipped(wb_rgb)*255))
+                    output_rgb = output_rgb.resize((int(output_rgb.width * resize_ratio),
+                        int(output_rgb.height * resize_ratio)), Image.ANTIALIAS)
+                    output_rgb.save("%s/%s/%s-%dx/out_rgb_%s.png"%(task_folder,mode,prefix,crop_ratio,save_prefix))
+
+                    input_camera_rgb = Image.fromarray(np.uint8(utils.clipped(cropped_input_rgb)*255))
+                    input_camera_rgb.save("%s/%s/%s-%dx/input_rgb_camera_orig_%s.png"%(task_folder,mode,prefix,crop_ratio,config_file["io"]["prefix"]))
+                    input_camera_rgb_naive = input_camera_rgb.resize((int(input_camera_rgb.width * up_ratio),
+                        int(input_camera_rgb.height * up_ratio)), Image.ANTIALIAS)
+                    input_camera_rgb_naive.save("%s/%s/%s-%dx/input_rgb_camera_naive_%s.png"%(task_folder,mode,prefix,crop_ratio,save_prefix), compress_level=1)
 
 
 if __name__ == "__main__":
